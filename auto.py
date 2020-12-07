897a634e777b2f1a8330bdb19c569618fd24d187
Brenner Haverlock <40365743+brennerhaverlock@users.noreply.github.com>
2020-12-07 09:14:38 -0500

    init

---
+++ auto.py
@@ -0,0 +1,21 @@
+#parent class
+class Car:
+
+    def __init__(self, speed, wheels):
+        self.speed = speed
+        self.wheels = wheels
+        
+    wheels = 4
+
+    def increaseSpeed(self):
+        self.speed += 1
+
+    
+    
+    
+
+     #Car
+     #SportsCar  
+     #Racecar
+     #Semi
+     
