487a5249830bf556a12786da8b125526481f16a9
Brenner Haverlock <40365743+brennerhaverlock@users.noreply.github.com>
2020-12-07 12:32:57 -0500

    swapped methods from electric to player class

--- electric.py
+++ electric.py
@@ -7,24 +7,6 @@ class Electric():
     balance = 100000
     cars = 0
     #The setWheels and setEngine are also inherited 
-    
-    def buyCar(self, price):
-        """Buy car"""
-        if self.balance > price:
-            self.balance -= price
-            self.cars += 1 
-            print("You've bought this car") 
-        else:
-            print("You don't have enough money to buy this car")
-
-
-    def sell_car(self, price):
-        """Sell car"""
-        if self.cars > 1:
-            self.balance += price
-            self.cars -= 1
-        else:
-            "You're unable to sell a car due to no cars"
 
     def start(self):
         print("Car started...Battery on")
